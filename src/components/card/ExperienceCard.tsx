645bbf0caf83073bcc994782f346d4040cd97f43
SXsid <sidshekhar2024@gmail.com>
2025-10-14 17:46:21 +0530

    bug fix

--- src/components/card/ExperienceCard.tsx
+++ src/components/card/ExperienceCard.tsx
@@ -1,48 +1,60 @@
-import React from 'react'
-import Image from 'next/image'
-import { IExperienceCard } from '@/Types/type'
-import { motion } from 'motion/react'
+import React from "react";
+import Image from "next/image";
+import { IExperienceCard } from "@/Types/type";
+import { motion } from "motion/react";
 
-function ExperienceCard({ positionTitle, companyName, from, to, imageURL, isFirst, isLast ,bgcolor}:IExperienceCard&{isFirst:boolean,isLast:boolean}) {
+function ExperienceCard({
+  positionTitle,
+  companyName,
+  from,
+  to,
+  imageURL,
+  isFirst,
+  isLast,
+  bgcolor,
+}: IExperienceCard & { isFirst: boolean; isLast: boolean }) {
   return (
     <motion.div
-    initial={{
-      x:-30,
-      opacity:0,
-      filter:"blur(3px)"
-    }}
-    whileInView={{
-      x:0,
-      opacity:1,
-      filter:"blur(0px)"
-    }}
-    viewport={{
-      once:true
-    }}
-    
-    className="flex py-4 pl-10 pr-2 sm:pl-12 relative">
-      
+      initial={{
+        x: -30,
+        opacity: 0,
+        filter: "blur(3px)",
+      }}
+      whileInView={{
+        x: 0,
+        opacity: 1,
+        filter: "blur(0px)",
+      }}
+      viewport={{
+        once: true,
+      }}
+      className="flex py-4 pl-10 pr-2 sm:pl-12 relative"
+    >
       <div className="flex  sm:flex-row w-full rounded-lg lg:px-4 py-4 ">
-       
         <div className=" lg:mr-4  mr-2 mb-3 sm:mb-0">
-          <div className="w-12 h-12 rounded-full overflow-hidden" style={{backgroundColor:bgcolor}}>
+          <div
+            className="w-12 h-12 rounded-full overflow-hidden"
+            style={{ backgroundColor: bgcolor }}
+          >
             {imageURL && (
-              <Image 
-                src={imageURL} 
-                alt={companyName} 
-                width={48} 
+              <Image
+                src={imageURL}
+                alt={companyName ? companyName : "freelance"}
+                width={48}
                 height={48}
-                className="w-full h-full object-contain" 
+                className="w-full h-full object-contain"
               />
             )}
           </div>
         </div>
-        
+
         {/* Content */}
         <div className=" lg:flex lg:justify-between lg:w-[80%]">
           <div>
-          <h3 className="text-lg font-light font-body   ">{positionTitle}</h3>
-          <h4 className="text-md text-neutral-400 font-sans font-light">{companyName}</h4>
+            <h3 className="text-lg font-light font-body   ">{positionTitle}</h3>
+            <h4 className="text-md text-neutral-400 font-sans font-light">
+              {companyName}
+            </h4>
           </div>
           <div className="text-sm hidden lg:block text-gray-500 dark:text-gray-400 mt-1">
             <span>{from}</span>
@@ -52,7 +64,8 @@ function ExperienceCard({ positionTitle, companyName, from, to, imageURL, isFirs
         </div>
       </div>
     </motion.div>
-  )
+  );
 }
 
-export default ExperienceCard
+export default ExperienceCard;
+
